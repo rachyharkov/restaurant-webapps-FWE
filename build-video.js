aec3ffa549d0594c404c3516c67c60b3654ce3f9
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2021-07-08 08:37:24 +0700

    ketinggalan

---
+++ build-video.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const FFmpeg = require('fluent-ffmpeg');
+ 
+ 
+const gify = new FFmpeg({ source: path.resolve(__dirname, 'src/public/images/***.gif') });
+ 
+ 
+gify.clone()
+ .withVideoCodec('libx264')
+ .withFps(25)
+ .toFormat('mp4')
+ .saveToFile(path.resolve(__dirname, 'src/public/images/***.mp4'));
+ 
+ 
+gify.clone()
+ .withFps(25)
+ .toFormat('webm')
+ .saveToFile(path.resolve(__dirname, 'src/public/images/***.webm'));
